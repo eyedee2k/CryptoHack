9c95fafe9da4d066ccbee02c5bfe8ddc44b19682
Ben Brown <benjamin.brown@workday.com>
2025-02-07 15:26:38 +0000

    commit

---
+++ SymmetricCiphers/SymmetricStarter/passwords_as_keys.py
@@ -0,0 +1,17 @@
+from Crypto.Cipher import AES
+import hashlib
+
+import requests
+url1 = "http://aes.cryptohack.org/passwords_as_keys/"
+r = requests.get(f"{url1}/encrypt_flag")
+data = r.json()
+c = data["ciphertext"]
+ciphertext = bytes.fromhex(c)
+with open("/Users/benjamin.brown/Documents/Uni/.venv/CryptoHack/SymmetricCyphers/SymmetricStarter/wordlist.txt") as f:
+    words = [w.strip() for w in f.readlines()]
+for i in words:
+    key = hashlib.md5(i.encode()).digest()
+    cipher = AES.new(key,AES.MODE_ECB)
+    decrypted = cipher.decrypt(ciphertext)
+    if b'crypto' in decrypted:
+        print(decrypted)
