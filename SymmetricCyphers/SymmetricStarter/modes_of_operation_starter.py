34a65b1e663e4e29bc3c27c6b3d68c110c9ee95e
Ben Brown <benjamin.brown@workday.com>
2025-02-07 11:57:45 +0000

    Create modes_of_operation_starter.py

---
+++ SymmetricCyphers/SymmetricStarter/modes_of_operation_starter.py
@@ -0,0 +1,3 @@
+# click encrypt_flag() to get the flag {"ciphertext":"a3c22d1bfcccd351b995f478a962da376a2cb1620cfda8c02aa6befcda831698"}
+# copy flag to decrypt(ciphertext) and get the plaintext {"plaintext":"63727970746f7b626c30636b5f633170683372355f3472335f663435375f217d"}
+# decode the plaintext using Hex Encoder/Decoder to get the flag crypto{bl0ck_c1ph3r5_4r3_f457_!}
